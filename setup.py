eab56842c15c34cf341cc8a5caa83bc88abad1e5
Vykstorm <victorruizgomezdev@gmail.com>
2018-04-25 14:04:21 +0200

    Modificada descripción de la librería

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name = 'regression_models',
     version = '1.0.0',
-    description = 'Python library wrapper utility around sklearn package to create regression based models',
+    description = 'Python library wrapper utility around sklearn package to create simple regression models',
     url = 'https://github.com/Vykstorm/regression_models',
     author = 'Vykstorm',
     author_email = 'victorruizgomezdev@gmail.com',
